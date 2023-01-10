997516d2e72af8176b61e02371354e3727594f89
OneAndOnlyDaniel <OneAndOnlyDaniel@protonmail.com>
2023-01-10 14:44:01 +0100

    Created Readme, Added addTestToFront.py

---
+++ README.md
@@ -0,0 +1,4 @@
+# Introduction
+This is a collection of personal tools I've made to help with different stuff,
+it's all for personal use so there is no error checking and the like, proceed
+with caution.
---
+++ addTextToFront.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+# String to be added
+stri = ""
+# If we want to filter by file extension
+filt = ""
+if(len(sys.argv) > 1):
+	stri = sys.argv[1]
+	if(len(sys.argv) > 2):
+		filt = sys.argv[2]
+	path = os.getcwd()
+	for root, directories, file in os.walk(path):
+		for file in file:
+			if(file.endswith(filt)):
+				os.rename(path + "/" + file, path + "/" + stri + file)
